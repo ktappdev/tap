6424ad527dc8a3670e53c9af91ae52d7c18dc55e
Ken Taylor <kentaylorappdev@gmail.com>
2026-06-04 18:32:10 -0400

    Add ssh-thing cask v1.1.23

---
+++ Casks/ssh-thing.rb
@@ -0,0 +1,17 @@
+cask "ssh-thing" do
+  version "1.1.23"
+  name "SSH Thing"
+  desc "SSH client"
+  homepage "https://github.com/ktappdev/ssh-thing"
+
+  on_arm do
+    url "https://github.com/ktappdev/ssh-thing/releases/download/v#{version}/SSH.THING_#{version}_aarch64.dmg"
+    sha256 "07183e5f007bfc8ab285983e6026c574faa4df3516e3315ab1599d93f24501f4"
+  end
+  on_intel do
+    url "https://github.com/ktappdev/ssh-thing/releases/download/v#{version}/SSH.THING_#{version}_x64.dmg"
+    sha256 "3bac78279b48f789da5eb1b5331a8f8957f9cf9aa800675ff117914b52b05016"
+  end
+
+  app "SSH Thing.app"
+end
